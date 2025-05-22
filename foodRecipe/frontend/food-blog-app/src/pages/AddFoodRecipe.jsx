71c1532925aeb8ab52d766e61799d74aab462c4e
sowjjon123 <145152171+sowjjon123@users.noreply.github.com>
2025-05-22 16:27:02 +0530

    Update AddFoodRecipe.jsx

--- foodRecipe/frontend/food-blog-app/src/pages/AddFoodRecipe.jsx
+++ foodRecipe/frontend/food-blog-app/src/pages/AddFoodRecipe.jsx
@@ -12,7 +12,7 @@ export default function AddFoodRecipe() {
     const onHandleSubmit = async (e) => {
         e.preventDefault()
         console.log(recipeData)
-        await axios.post("http://localhost:5000/recipe", recipeData,{
+        await axios.post("http://https://foodrecipeapp-1.onrender.com/recipe", recipeData,{
             headers:{
                 'Content-Type':'multipart/form-data',
                 'authorization':'bearer '+localStorage.getItem("token")
