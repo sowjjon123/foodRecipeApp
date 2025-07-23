9141557ca9778a951a11de1ba498496a16e44723
sowjjon <sowjjon@gmail.com>
2025-07-23 19:48:13 +0530

    image

--- foodRecipe/frontend/food-blog-app/src/pages/Home.jsx
+++ foodRecipe/frontend/food-blog-app/src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-import foodRecipe from '../assets/foodRecipe.png'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import foodRecipe from '/foodRecipe.png'
 import RecipeItems from '../components/RecipeItems'
 import { useNavigate } from 'react-router-dom'
 import Modal from '../components/Modal'
